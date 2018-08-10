35d1800a20b07c03417f3bc390b39f5e400d1138
Trace Ohrt <teohrt18@gmail.com>
2018-08-10 03:07:50 -0500

    Started frontend development and modified directory structure

--- blockchainAPI/app.js
+++ blockchainAPI/app.js
@@ -1,14 +1,14 @@
-var users = [];              // Stores blockchain accounts
+var users = [];                 // Stores blockchain accounts
 var contractAddressList = [];   // Stores deployed smart contract addresses with their ABI definition
 var chainPort   = 8545;         // Geth's default value
-var serverPort  = 3333;         // Arbitrary port of my liking
+var serverPort  = 3000;         // Arbitrary port of my liking
 var gasAmount   = 9999999;      // High number for easy transactions on my devnet
 
 var express = require('express');
 var app = express();
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:" + chainPort));
-var apiController = require('./BlockchainDEVNET/controllers/apiController');
+var apiController = require('./controllers/apiController');
 
 // Routes
 apiController(app, web3, contractAddressList, gasAmount, users);
